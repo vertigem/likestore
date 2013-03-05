8d9b986f8dd057e35f57db69151c842ad5439fcf
Duke <duke@riseup.net>
2013-03-05 02:32:20 -0300

    added productos attrs

--- lib/likestore/product.rb
+++ lib/likestore/product.rb
@@ -2,5 +2,17 @@ require 'likestore/identity'
 
 module Likestore
   class Product < Likestore::Identity
+    def description
+      @attrs[:descricao]
+    end
+    def title
+      @attrs[:nome]
+    end
+    def sku
+      @attrs[:sku]
+    end
+    def weigth
+      @attrs[:peso]
+    end
   end
 end
